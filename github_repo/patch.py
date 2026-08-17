5a1bb6ec2dc2ec3c4943b6a583bb80b58dfd1215
亗 𝐉𝐨𝐬𝐡𝐮𝐚 <joshua.m.lucian@gmail.com>
2026-08-18 04:05:49 +0630

    Fix uvloop import logic and indentation in patch.py
    Refactor the dependency check for uvloop and fix indentation.

--- github_repo/patch.py
+++ github_repo/patch.py
@@ -1,19 +1,16 @@
 src = open('netwarrior.py', encoding='utf-8').read()
 
-src = src.replace(
-    'import uvloop',
-    'import sys as _sys\nif _sys.platform != "win32":\n    import uvloop\n    _UVLOOP = True\nelse:\n    _UVLOOP = False'
-)
+# Fix the broken check_deps indentation
+bad = '''    _deps = ["rich", "scapy", "psutil", "paramiko", "dns", "aiohttp", "tomli", "tomli_w"]
+if _sys.platform != "win32":
+    _deps.append("uvloop")
+for pkg in _deps:'''
 
-src = src.replace(
-    'for pkg in ["rich", "scapy", "psutil", "paramiko", "dns", "aiohttp", "tomli", "tomli_w", "uvloop"]:',
-    '_deps = ["rich", "scapy", "psutil", "paramiko", "dns", "aiohttp", "tomli", "tomli_w"]\nif _sys.platform != "win32":\n    _deps.append("uvloop")\nfor pkg in _deps:'
-)
+good = '''    _deps = ["rich", "scapy", "psutil", "paramiko", "dns", "aiohttp", "tomli", "tomli_w"]
+    if _sys.platform != "win32":
+        _deps.append("uvloop")
+    for pkg in _deps:'''
 
-src = src.replace(
-    '    uvloop.install()\n    try:\n        asyncio.run(main())',
-    '    if _UVLOOP:\n        uvloop.install()\n    try:\n        asyncio.run(main())'
-)
-
+src = src.replace(bad, good)
 open('netwarrior.py', 'w', encoding='utf-8').write(src)
-print('Done. Run: python netwarrior.py')
+print('Fixed. Run: python netwarrior.py')
